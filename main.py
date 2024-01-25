10094af5cc66d7f347a44a7ab0d0168e2f1523dc
JorgeCabrera19 <149099310+JorgeCabrera19@users.noreply.github.com>
2024-01-25 08:49:56 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-dolar = float(input("Ingrese cantidad en dolares: "))
+A = float(input("Ingrese el valor de la variable A"))
+B = float(input("Ingrese el valor de la variable B"))
+C = float(input("Ingrese el valor de la variable C"))
 
-quetzal = dolar * 7.83
+raiz = float(((B*B)-4*A*C)**0.5)
 
-print(quetzal," Quetzales")
+pos = float((-B + raiz)/2*A)
+
+neg = float((-B - raiz)/2*A)
+
+print("Los resultados son x:", pos, "x2:", neg)
